bec449068da8076bb0da26c09d6d2061e2683ebe
Grace Alele <98232863+The-Tech-Sis@users.noreply.github.com>
2025-09-26 16:32:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
-
+import pandas as pd
+import numpy as np
+from scikitlearn import 
 st.title('🎈 Machine Learning Project')
 
 st.info('This app is for a machine learning deployed model')
+
+with st.explander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv['https://raw.githubusercontent.com/dataprofessor/streamlit_freecodecamp/refs/heads/main/app_8_classification_penguins/penguins_cleaned.csv']
+  df
+
+  st.write('**x**')
+  x = df.drop('species', asix=1)
+  x
+
+  st.write('**y**')
+  y = df.species
+  y
