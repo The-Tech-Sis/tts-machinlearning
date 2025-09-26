a380d6979aad1e28717e489e34c69750cc6fa5af
Grace Alele <98232863+The-Tech-Sis@users.noreply.github.com>
2025-09-26 16:45:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,30 @@
+#Imports
 import streamlit as st
 import pandas as pd
 import numpy as np
-from scikitlearn import 
-st.title('🎈 Machine Learning Project')
+#from scikitlearn import 
 
+st.title('🎈 Machine Learning Project')
 st.info('This app is for a machine learning deployed model')
 
-with st.explander('Data'):
+
+with st.expander('Data'):
   st.write('**Raw data**')
   df = pd.read_csv['https://raw.githubusercontent.com/dataprofessor/streamlit_freecodecamp/refs/heads/main/app_8_classification_penguins/penguins_cleaned.csv']
   df
 
   st.write('**x**')
-  x = df.drop('species', asix=1)
+  x = df.drop('species', axis=1)
   x
 
   st.write('**y**')
   y = df.species
   y
+
+#Data Visualization
+with st.expander('Data Visualization'):
+#"species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+
+#Data Preparation
