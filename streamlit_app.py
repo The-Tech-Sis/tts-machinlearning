57be06845db22b0472942ced56a4f5ec9300e5a0
Grace Alele <98232863+The-Tech-Sis@users.noreply.github.com>
2025-09-27 07:33:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ with st.expander('Data'):
 with st.expander('Data Visualization 1'):
 #"species","island","bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
-  st.area_chart(data=df, x='species', y='island')
+  st.area_chart(data=df, x='species', y='island', color='species')
 
 
 #Data Preparation
